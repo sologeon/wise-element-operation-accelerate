fb887d5cffae7583b4c3a127ea2a229bcd089a47
woodstock-xkx <xkx@trapdoortech.com>
2020-06-12 20:32:15 +0800

    feat: use OpenMP to parallelize

--- ori-code/main.cpp
+++ ori-code/main.cpp
@@ -11,10 +11,12 @@ int main(){
   std::default_random_engine e{r()};
   std::uniform_real_distribution<float> u(-1, 1);
 
-  int size = 10000;
-  double vec[size];
+  int size = 1<<26;
+  std::vector<double> vec(size);
+  #pragma omp parallel for
   for (int i = 0; i < size; ++i)
       vec[i] = u(e);
+
   for (int i = 0; i < 1000; ++i){
     vec[rand()%size] = NAN;
     vec[rand()%size] = log(0);
@@ -24,12 +26,14 @@ int main(){
   double diff;
 
   gettimeofday(&start, nullptr);
-  bool res[size];
-  for (int j = 0; j < 10000; ++j){
-    for (int i = 0; i < size; ++i){
+  std::vector<bool> res(size);
+  // for (int j = 0; j < 10000; ++j){
+ #pragma omp parallel for
+    for (int i = 0; i < size; ++i)
+    {
       res[i] = isfinite(vec[i]);
     }
-  }
+  // }
   gettimeofday(&end, nullptr);
   diff = end.tv_sec-start.tv_sec+(end.tv_usec-start.tv_usec)/1e6;
   cout << endl << "comsume: " << diff << " s" << endl;
